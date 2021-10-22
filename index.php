007c468ba2c2be85c1ff29df4d645e369bad4cc4
余延平 <mr.yu1991@gmail.com>
2021-10-22 23:50:09 +0800

    feat: 注册WordPress主题

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * The main template file
+ *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package SEOZEN 
+ * @subpackage SEOZEN_Dummy
+ * @since SEOZEN Dummy 1.0
+ */
+
+
+echo "我是index.php";
