80aae5a34e0a840a12e5cd8052972f4771e8eda0
余延平 <mr.yu1991@gmail.com>
2021-12-26 22:32:04 +0800

    05-自定义头部导航-register_nav_menu

--- functions.php
+++ functions.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Functions and definitions
  *
@@ -19,15 +20,28 @@
 function seozen_scripts() {
 
 	// seozen styles.
-	wp_enqueue_style( 'seozen-style', get_template_directory_uri() . '/assets/css/seozen.css', array(), wp_get_theme()->get( 'Version' ));
+	wp_enqueue_style('seozen-style', get_template_directory_uri() . '/assets/css/seozen.css', array(), wp_get_theme()->get('Version'));
 
 	// seozen script.
 	wp_enqueue_script(
 		'seozen-script',
 		get_template_directory_uri() . '/assets/js/seozen.js',
 		array(),
-		wp_get_theme()->get( 'Version' ),
+		wp_get_theme()->get('Version'),
 		true
 	);
 }
-add_action( 'wp_enqueue_scripts', 'seozen_scripts' );
+add_action('wp_enqueue_scripts', 'seozen_scripts');
+
+/**
+ * 添加WordPress自定义菜单支持
+ * 
+ * @return void
+ */
+function register_seozen_dunmmy_menu() {
+	// header-menu是这个菜单放在什么位置的标识
+	// 描述字段会显示在后台自定义菜单面板下方，需要选中保存才会生效
+	register_nav_menu('header-menu', '我是自定义菜单位置描述');
+	// register_nav_menus注册多个自定义菜单
+}
+add_action('init', 'register_seozen_dunmmy_menu');
--- header.php
+++ header.php
@@ -1,11 +1,18 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <?php wp_head() ?>
 </head>
-<body>
 
-<p class="header">我是header.php</p>
+<body>
+    <?php
+    wp_nav_menu(
+        array(
+            "theme_location" => "header-menu"
+        )
+    );
+    ?>
